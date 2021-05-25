c4e1ebcb503f79d950d7cc5bba8747a1622003ce
Piotr Domian <domianpiotr111@gmail.com>
2021-05-25 20:27:57 +0200

    Project done

---
+++ js/main.js
@@ -0,0 +1,61 @@
+let score = 0
+currency = " zł";
+
+fetch("http://api.nbp.pl/api/exchangerates/tables/c/")
+.then(data => data.json())
+.then(data => {
+    currencies = data[0]
+    console.log(currencies);
+    const input = document.querySelector("input")
+    const select = document.getElementById("select");
+    const button = document.querySelector(".btn");
+    const scoreTxt = document.querySelector(".score");
+    
+    button.addEventListener("click", (e) =>{
+        if(input.value === 0 || input.value === null)
+        {
+            error.textContent = "Please put a proper value!";
+        }
+        else
+        {
+            if(select.value === "EUR")
+            {
+                score = currencies.rates[3].ask * input.value;
+            }
+            else if (select.value === "USD")
+            {
+                score = currencies.rates[0].ask * input.value;
+            }
+            else
+            {
+                score = currencies.rates[5].ask * input.value;
+            }
+            scoreTxt.innerHTML = score.toFixed(2) + currency;
+        }
+    });
+    
+    select.addEventListener("input", (e) =>{
+        if(input.value === 0 || input.value === null)
+        {
+            error.textContent = "Please put a proper value!";
+        }
+        else
+        {
+            if(select.value === "EUR")
+            {
+                score = currencies.rates[3].ask * input.value;
+            }
+            else if (select.value === "USD")
+            {
+                score = currencies.rates[0].ask * input.value;
+            }
+            else
+            {
+                score = currencies.rates[5].ask * input.value;
+            }
+        }   
+        scoreTxt.innerHTML = score.toFixed(2) + currency;
+    });
+})
+.catch(err => console.log(err));
+
--- main.js
+++
@@ -1,7 +0,0 @@
-fetch("http://api.nbp.pl/api/exchangerates/tables/c/")
-.then(data => data.json())
-.then(data => {
-    console.log(data)
-})
-.catch(err => console.log(err));
-
